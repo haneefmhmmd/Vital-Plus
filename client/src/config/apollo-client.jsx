00d386e1c0e4e6a61beaa8b79bd5a0c7b632b326
haneefmhmmd <haneefmhmmd@gmail.com>
2024-04-13 05:06:28 -0400

    added authentication ui

--- client/src/config/apollo-client.jsx
+++ client/src/config/apollo-client.jsx
@@ -24,4 +24,12 @@ export const REGISTER_USER = gql`
     }
   }
 `;
+
+export const LOGIN_USER = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`;
 export default client;
--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,42 +1,63 @@
 import { useMutation } from "@apollo/client";
 import { useForm } from "react-hook-form";
-import { useHistory } from "react-router-dom";
-import Button from "../components/Button";
-import { REGISTER_USER } from "../config/apollo-client";
-export default function Auth() {
-  const history = useHistory();
-  const { register, handleSubmit } = useForm();
-  const [registerMutateFunction, { data, loading, error }] =
-    useMutation(REGISTER_USER);
 
-  const onSubmit = async (data) => {
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
+import { LOGIN_USER, REGISTER_USER } from "../config/apollo-client";
+
+import { saveToLS } from "../utils/localStorage.util";
+
+export default function Auth() {
+  const navigate = useNavigate();
+  const { register, handleSubmit } = useForm();
+  const [registerMutateFunction] = useMutation(REGISTER_USER);
+
+  const [loginMutateFunction] = useMutation(LOGIN_USER);
+
+  const onRegister = async (data) => {
     const { firstName, lastName, email, password } = data;
 
     try {
-      const { data } = await registerMutateFunction({
+      const { registerData } = await registerMutateFunction({
         variables: { firstName, lastName, email, password },
       });
 
-      if (data && data.registerMutateFunction) {
-        history.push("/app");
+      if (registerData) {
+        navigate("/login");
       }
     } catch (error) {
       console.error("Error adding admin:", error);
     }
   };
+
+  const onLogin = async (data) => {
+    console.log("Logging in...", data);
+    const { email, password } = data;
+
+    try {
+      const { data: responseData } = await loginMutateFunction({
+        variables: { email, password },
+      });
+
+      if (responseData && responseData.login) {
+        saveToLS("token", responseData.login.token);
+        navigate("/app");
+      }
+    } catch (error) {
+      console.error("Error logging in:", error);
+    }
+  };
+
   return (
     <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form>
         <div>
           <label htmlFor="firstName">First Name</label>
-          <input
-            {...register("firstName", { required: true })}
-            id="firstName"
-          />
+          <input {...register("firstName")} id="firstName" />
         </div>
         <div>
           <label htmlFor="lastName">Last Name</label>
-          <input {...register("lastName", { required: true })} id="lastName" />
+          <input {...register("lastName")} id="lastName" />
         </div>
 
         <div>
@@ -48,7 +69,8 @@ export default function Auth() {
           <label htmlFor="password">Your Password</label>
           <input {...register("password", { required: true })} id="password" />
         </div>
-        <Button label="Register" />
+        <Button label="Register" onClick={handleSubmit(onRegister)} />
+        <Button label="Login" onClick={handleSubmit(onLogin)} />
       </form>
     </div>
   );
