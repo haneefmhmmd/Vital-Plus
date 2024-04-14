a7cc491c4fe2e9be5d69fc5735afca63cd5a85d4
Deanne Laylay <deannelaylay@gmail.com>
2024-04-14 18:24:44 -0400

    updated Patient

--- client/src/pages/nurse/Patient.jsx
+++ client/src/pages/nurse/Patient.jsx
@@ -46,27 +46,39 @@ export default function Patient() {
   }
 
   return (
-    <article>
-      {data && (
-        <div>
-          <div>
-            <h1>
+    <>
+      <header className="dashboard-header">
+        <div className="header__left">
+          <img
+            className="profile-img"
+            src={data.patient.image}
+            alt={`Profile image of ${data.patient.firstName}`}
+          />
+          <div className="header__info">
+            <h1 className="title">
               {data.patient.firstName} {data.patient.lastName}
             </h1>
             <p>
               {data.patient.email} | {data.patient.phoneNumber}
             </p>
           </div>
-          <div>
-            <Button label="Add Vital" elementType="link" href="/add-vital" />
-            <Button
-              label="Provide Consultation"
-              elementType="link"
-              href="/nurse/consultation"
-            />
-          </div>
         </div>
-      )}
+        <div className="header__right">
+          <Button
+            variant="secondary"
+            label="Add Vitals"
+            elementType="link"
+            href="/add-vital"
+          />
+          <Button
+            variant="secondary"
+            label="Provide Consultation"
+            elementType="link"
+            href="/nurse/consultation"
+          />
+        </div>
+      </header>
+
       {vitalsLoading && (
         <div>
           <p>Loading patient vitals info...</p>
@@ -79,44 +91,46 @@ export default function Patient() {
       )}
       {vitals && (
         <>
-          <h3>Vitals</h3>
-          <table>
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>Body Temperature</th>
-                <th>Body Pressure</th>
-                <th>Respiratory Rate</th>
-                <th>Weight</th>
-                <th>Symptoms</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              {vitals.vitalByPatientId.length !== 0 &&
-                vitals.vitalByPatientId[0].measurements.map((measurement) => {
-                  return (
-                    <tr key={uuid()}>
-                      <td>{measurement.date}</td>
-                      <td>{measurement.bodyTemperature}</td>
-                      <td>{measurement.bodyPressure}</td>
-                      <td>{measurement.respiratoryRate}</td>
-                      <td>{measurement.weight}</td>
-                      <td>{measurement.symptoms.join(", ")}</td>
-                    </tr>
-                  );
-                })}
-              {vitals.vitalByPatientId.length == 0 && (
-                <tr>
-                  <td colSpan="6" align="center">
-                    No Vital Measurements available
-                  </td>
+          <h2 className="sub-title mt-4">History</h2>
+          <div className="rounded shadow-sm p-2 bg-white table-responsive mt-3">
+            <table className="table table-borderless table-hover align-middle">
+              <thead>
+                <tr className="border-bottom">
+                  <th>Date</th>
+                  <th>Body Temperature</th>
+                  <th>Body Pressure</th>
+                  <th>Respiratory Rate</th>
+                  <th>Weight</th>
+                  <th>Symptoms</th>
                 </tr>
-              )}
-            </tbody>
-          </table>
+              </thead>
+
+              <tbody>
+                {vitals.vitalByPatientId.length !== 0 &&
+                  vitals.vitalByPatientId[0].measurements.map((measurement) => {
+                    return (
+                      <tr key={uuid()}>
+                        <td>{measurement.date}</td>
+                        <td>{measurement.bodyTemperature}</td>
+                        <td>{measurement.bodyPressure}</td>
+                        <td>{measurement.respiratoryRate}</td>
+                        <td>{measurement.weight}</td>
+                        <td>{measurement.symptoms.join(", ")}</td>
+                      </tr>
+                    );
+                  })}
+                {vitals.vitalByPatientId.length == 0 && (
+                  <tr>
+                    <td colSpan="6" align="center">
+                      No Vital Measurements available
+                    </td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
         </>
       )}
-    </article>
+    </>
   );
 }
